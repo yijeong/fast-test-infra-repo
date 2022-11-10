72c22cca7419be0c4788dc2f135038abeb66db57
nessa.829 <nessa.829@kakaostyle.com>
2022-11-10 21:20:04 +0900

    clear repo

--- backend.tf
+++
@@ -1,9 +0,0 @@
-# tfstate를 저장할 backend 설정
-terraform {
-  backend "s3" {
-    bucket  = "nessa-tfstate"
-    key     = "terraform.state"
-    region  = "ap-northeast-2"
-    profile = "playground"
-  }
-}
--- provider.tf
+++
@@ -1,6 +0,0 @@
-# aws provider 설정
-
-provider "aws" {
-  profile = "playground"
-  region  = "ap-northeast-2"
-}
