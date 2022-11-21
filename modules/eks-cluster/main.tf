30636c41d9f64737387de0c0eae1f3dd1eb9557f
nessa.829 <nessa.829@kakaostyle.com>
2022-11-21 17:58:34 +0900

    add public subnet tag

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -170,3 +170,25 @@ resource "aws_ec2_tag" "private_subnet_karpenter_tag" {
   key         = "karpenter.sh/discovery/${local.cluster_name}"
   value       = local.cluster_name
 }
+
+// 퍼블릭 서브넷 태그
+resource "aws_ec2_tag" "public_subnet_tag" {
+for_each    = toset(local.public_subnets)
+resource_id = each.value
+key         = "kubernetes.io/role/elb"
+value       = "1"
+}
+
+resource "aws_ec2_tag" "public_subnet_cluster_tag" {
+for_each    = toset(local.public_subnets)
+resource_id = each.value
+key         = "kubernetes.io/cluster/${local.cluster_name}"
+value       = "owned"
+}
+
+resource "aws_ec2_tag" "public_subnet_karpenter_tag_2" {
+for_each    = toset(local.public_subnets)
+resource_id = each.value
+key         = "karpenter.sh/discovery/${local.cluster_name}"
+value       = local.cluster_name
+}
